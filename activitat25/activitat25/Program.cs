f39fead0b22f4fe38c574031aead392ea94e60fc
sandroAntonucci <sandro.antonucci.7e7@itb.cat>
2023-11-05 11:19:07 +0100

    update

--- activitat25/activitat25/Program.cs
+++ activitat25/activitat25/Program.cs
@@ -2,7 +2,7 @@
 *Author: Sandro Antonucci Rejón
  * M03. Programació UF1
  * v1. 05/11/2023
- * Exercici X. Enunciat de l’exercici que es resol
+ * Exercici 25. Implementa un programa que emmagatzemi una frase introduïda per teclat en la variable textOne. Demana un altre text i emmagatzema’l en la variable textTwo. Afegeix textTwo a textOne i mostra-la per pantalla. És correcte? Què pots observar a les variables textOne i textTwo?
  *
  */
 
@@ -12,19 +12,28 @@ using System;
 namespace MyApplication
 {
 
-    class Hola
+    class Texts
     {
         static void Main()
         {
 
-            const string MsgNum = "Introdueix un nombre enter: ";
+            const string MsgTextOne = "Introdueix una frase: ";
+            const string MsgTextTwo = "Introdueix una altra frase: ";
             const string MsgEnd = "\nPrem una tecla per continuar.";
 
-            int num;
+            string textOne;
+            string textTwo;
+            string text;
 
-            Console.Write(MsgNum);
-            num = Convert.ToInt32(Console.ReadLine());
+            Console.Write(MsgTextOne);
+            textOne = Console.ReadLine();
 
+            Console.Write(MsgTextTwo);
+            textTwo = Console.ReadLine();
+
+            text = textOne + textTwo;
+
+            Console.WriteLine(text);
 
             Console.WriteLine(MsgEnd);
             Console.ReadKey();
