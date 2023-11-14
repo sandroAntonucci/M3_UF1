b290ddb7a8b32e5601f62880b061daaa263a6cb3
sandroAntonucci <sandro.antonucci.7e7@itb.cat>
2023-11-14 21:51:08 +0100

    update

---
+++ Pràctica Examen/pr1/pr1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MyApplication
+{
+
+    class Matriu
+    {
+        static void Main()
+        {
+
+            int[] matrix = {4,1,2,5,7};
+
+            for(int i = 0; i < matrix.Length-1; i++)
+            {
+                for (int j = i+1;  j < matrix.Length; j++)
+                {
+                    if (matrix[i] > matrix[j])
+                    {
+                        int aux = matrix[i];
+                        matrix[i] = matrix[j];
+                        matrix[j] = aux;
+                    }
+                }
+            }
+
+        }
+    }
+
+
+}
