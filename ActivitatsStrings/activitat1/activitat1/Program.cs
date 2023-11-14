74efe8764d8b0a3ee23050a828019e695c3e736a
sandroAntonucci <sandro.antonucci.7e7@itb.cat>
2023-11-14 21:28:54 +0100

    update

---
+++ ActivitatsStrings/activitat1/activitat1/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Author: Sandro Antonucci Rejón
+ * M03. Programació UF1
+ * v1. 14/11/2023
+ * Exercici 1. Implementa un programa que demani una cadena a l’usuari i retorni si aquesta és palíndroma o no. 
+ */
+
+
+using System;
+
+namespace MyApplication
+{
+
+    class Palindrome
+    {
+        static void Main()
+        {
+
+            const string MsgText = "Introdueix una cadena: ";
+            const string MsgEnd = "\nPrem una tecla per continuar.";
+
+            string text, reverseText = "";
+
+            Console.Write(MsgText);
+            text = Console.ReadLine();
+
+            for(int i = text.Length; i > 0; i--)
+            {
+                reverseText += text[i-1];
+            }
+
+            Console.WriteLine(text.Equals(reverseText) ? "És palíndrom" : "No és palíndrom");
+            
+            Console.WriteLine(MsgEnd);
+            Console.ReadKey();
+        }
+    }
+
+}
---
+++ ActivitatsStrings/activitat4/activitat4/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Author: Sandro Antonucci Rejón
+ * M03. Programació UF1
+ * v1. 14/11/2023
+ * Exercici 4. Implementa un programa que demani una cadena i compti el nombre de caràcters, dígits i caràcters especials en aquesta cadena.
+ *
+ */
+
+
+using System;
+
+namespace MyApplication
+{
+
+    class Special
+    {
+        static void Main()
+        {
+
+            const string MsgText = "Introdueix un text: ";
+            const string MsgEnd = "\nPrem una tecla per continuar.";
+
+            int charact = 0, specialCount = 0, digitsCount = 0;
+
+            string text, special="$%&/()=?¿*", digits="0123456789";
+
+            Console.Write(MsgText);
+            text = Console.ReadLine();
+
+            for(int i = 0; i < text.Length; i++)
+            {
+                if (special.Contains(text[i])) specialCount++;
+
+                else if (digits.Contains(text[i])) digitsCount++;
+
+                else charact++;
+            }
+
+            Console.WriteLine("Caràcters: " + charact);
+            Console.WriteLine("Dígits: " + digitsCount);
+            Console.WriteLine("Special: " + specialCount);
+
+
+            Console.WriteLine(MsgEnd);
+            Console.ReadKey();
+        }
+    }
+
+}
