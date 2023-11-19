a32f8a7d42394933e93a50f3511436923be7756a
sandroAntonucci <sandro.antonucci.7e7@itb.cat>
2023-11-19 20:01:53 +0100

    update

--- PR2/penjat/penjat/Program.cs
+++ PR2/penjat/penjat/Program.cs
@@ -18,27 +18,93 @@ namespace MyApplication
 
             const string MsgInput = "\n>> ";
             const string MsgWelcome = "\t\t\t******************************\n\t\t\t****Bienvenid@ al ahorcado****\n\t\t\t******************************";
-            const string MsgDifficulty = "\t\tPor favor, escoge el nivel de dificultad: \n\n";
+            const string MsgDifficulty = "\n\t\tPor favor, escoge el nivel de dificultad: \n\n";
             const string MsgDifficultiesList = "\t\tA. Fácil\n\t\tB. Normal\n\t\tC. Difícil\n\t\tD. Experto";
+            const string MsgDifficultyChosen = "\n\t\t¡Has escogido el nivel de dificultad {0}! ";
+            const string MsgText = "Introdueix un text per a escollir la paraula: ";
             const string MsgEnd = "\nPrem una tecla per continuar.";
 
-            int difficulty;
+            string difficulty = "", text = "";
 
             //Intro - Bienvenido al ahorcado
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine(MsgWelcome);
-            Console.ReadLine();
 
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine(MsgDifficulty);
             Console.ForegroundColor = ConsoleColor.Magenta;
             Console.WriteLine(MsgDifficultiesList);
             Console.Write(MsgInput);
-            Console.ReadLine();
+            difficulty = Console.ReadLine();
+
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            //Switch para la dificultad (escoge una palabra de mas o menos longitud)
+
+            switch (difficulty.ToLower()){
+
+
+                //Modo Fácil
+                case "a":
+
+                    //Introducció del text
+                    Console.WriteLine(MsgDifficultyChosen, "fácil");
+                    Console.ReadLine();
+                    Console.WriteLine(MsgText);
+                    Console.WriteLine(MsgInput);
+                    text = Console.ReadLine();
+
+
+
+                    break;
+
+                //Modo Normal
+                case "b":
+
+                    //Introducció del text
+                    Console.WriteLine(MsgDifficultyChosen, "normal");
+                    Console.ReadLine();
+                    Console.WriteLine(MsgText);
+                    Console.WriteLine(MsgInput);
+                    text = Console.ReadLine();
+
+
+
+                    break;
+
+                //Modo Difícil
+                case "c":
+
+                    //Introducció del text
+                    Console.WriteLine(MsgDifficultyChosen, "difícil");
+                    Console.ReadLine();
+                    Console.WriteLine(MsgText);
+                    Console.WriteLine(MsgInput);
+                    text = Console.ReadLine();
+
+                    break;
+
+
+                //Modo Experto
+                case "d":
+
+                    //Introducció del text
+                    Console.WriteLine(MsgDifficultyChosen, "experto");
+                    Console.ReadLine();
+                    Console.WriteLine(MsgText);
+                    Console.WriteLine(MsgInput);
+                    text = Console.ReadLine();
+
+                    break;
+
+            }
+
+
 
 
             Console.WriteLine(MsgEnd);
             Console.ReadKey();
+
         }
     }
 
