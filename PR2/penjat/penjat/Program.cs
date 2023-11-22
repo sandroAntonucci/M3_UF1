19ea6f3843a31f295718aa22cdc0294d5491ac6a
sandroAntonucci <sandro.antonucci.7e7@itb.cat>
2023-11-22 17:25:16 +0100

    update

--- PR2/penjat/penjat/Program.cs
+++ PR2/penjat/penjat/Program.cs
@@ -1,5 +1,4 @@
-﻿/*
- * Author: Sandro Antonucci Rejón
+﻿*Author: Sandro Antonucci Rejón
  * M03. Programació UF1
  * v1. 13/11/2023
  * PR2. En aquesta pràctica, creareu la nostra versió de “El joc del Penjat”, on l’usuari haurà d’endevinar la paraula escollida pel programa, segons el nivell de dificultat.
@@ -23,12 +22,15 @@ namespace MyApplication
             const string MsgDifficulty = "\n\t\tPor favor, escoge el nivel de dificultad: \n\n";
             const string MsgDifficultiesList = "\t\tA. Fácil\n\t\tB. Normal\n\t\tC. Difícil\n\t\tD. Experto";
             const string MsgDifficultyChosen = "\n\t\t¡Has escogido el nivel de dificultad {0}! ";
-            const string MsgText = "Introduce un texto para escoger una palabra: ";
+            const string MsgText = "\t\tIntroduce un texto para escoger una palabra: ";
+            const string MsgTries = "\tNúmero de intentos restantes: {0}";
             const string MsgContinue = "\n\t\tPulsa una tecla para continuar.";
 
             int difficultyTries = 3, textTries = 3, tries;
 
             string difficulty = "", text = "", hangmanWord;
+
+            bool gameLost = false, wordFound = false;
 
             //Intro - Bienvenido al ahorcado
             Console.ForegroundColor = ConsoleColor.Green;
@@ -40,7 +42,7 @@ namespace MyApplication
             {
 
                 //Informa al usuario en caso de valor no válido
-                if(difficultyTries < 3)
+                if (difficultyTries < 3)
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine(MsgNotValid);
@@ -62,7 +64,7 @@ namespace MyApplication
 
 
             //Comprueba si el usuario se ha quedado sin intentos
-            
+
             if (difficultyTries == 0)
             {
                 Console.ForegroundColor = ConsoleColor.Red;
@@ -110,6 +112,7 @@ namespace MyApplication
                 do
                 {
                     Console.WriteLine(MsgText);
+                    Console.ForegroundColor = ConsoleColor.Magenta;
                     Console.Write(MsgInput);
                     text = Console.ReadLine();
                     textTries--;
@@ -120,12 +123,12 @@ namespace MyApplication
             text = text.Replace("á", "a").Replace("é", "e").Replace("í", "i").Replace("ó", "o").Replace("ú", "u").Replace("Á", "A").Replace("É", "E").Replace("Í", "I").Replace("Ó", "O").Replace("Ú", "U");
 
             //Separa cada palabra en un array
-            string[] words = text.Split(' '); 
+            string[] words = text.Split(' ');
 
             //S'ordena l'array de menor a major
-            for(int i = 0; i < words.Length - 1; i++)
+            for (int i = 0; i < words.Length - 1; i++)
             {
-                for(int j = i+1; j < words.Length; j++)
+                for (int j = i + 1; j < words.Length; j++)
                 {
                     if (words[i].Length > words[j].Length)
                     {
@@ -136,14 +139,10 @@ namespace MyApplication
                 }
             }
 
-            foreach(string s in words)
-            {
-                Console.WriteLine(s);
-            }
-
 
             //Switch para la dificultad (escoge una palabra de mas o menos longitud)
-            switch (difficulty){
+            switch (difficulty)
+            {
 
 
                 //Modo Fácil - Se escoge la palabra más corta del texto
@@ -155,7 +154,7 @@ namespace MyApplication
 
 
                     break;
-                    
+
                 //Modo Normal - Se escoge la última palabra del primer cuarto del texto ordenado por longitud (dividimos la longitud del array por 4)
                 case "b":
 
@@ -180,7 +179,7 @@ namespace MyApplication
 
                     tries = 3;
 
-                    hangmanWord = words[words.Length-1];
+                    hangmanWord = words[words.Length - 1];
 
                     break;
 
